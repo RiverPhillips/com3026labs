db9ad5172e35b1e524a83a7009fe5cd4e7344e3a
River Phillips <riverphillips1@gmail.com>
2020-05-19 13:11:50 +0100

    update interface in test script
    Signed-off-by: River Phillips <riverphillips1@gmail.com>

--- cw/src/test.exs
+++ cw/src/test.exs
@@ -1,19 +1,16 @@
-IEx.Helpers.c("paxos.exs")
+IEx.Helpers.c("paxos.ex")
 
-:global.unregister_name(:upper)
-:global.register_name(:upper, self())
+pid1 = Paxos.start(:p1, [:p1, :p2, :p3], self())
+pid2 = Paxos.start(:p2, [:p1, :p2, :p3], self())
+pid3 = Paxos.start(:p3, [:p1, :p2, :p3], self())
 
-pid1 = Paxos.start(:p1, [:p1, :p2, :p3], :upper)
-pid2 = Paxos.start(:p2, [:p1, :p2, :p3], :upper)
-pid3 = Paxos.start(:p3, [:p1, :p2, :p3], :upper)
+for {p, v} <- [{pid1, :a}, {pid2, :b}, {pid3, :c}], do: Paxos.propose(p, v)
 
-for {p, v} <- [{:p1, :a}, {:p2, :b}, {:p3, :c}], do: Paxos.propose(p, v)
-
-Paxos.start_ballot(:p1)
+Paxos.start_ballot(pid1)
 
 for _ <- 1..3 do
   receive do
-    {:decided, value} ->
+    {:decide, value} ->
       IO.puts(value)
   after
     10000 -> IO.puts("Timeout")
